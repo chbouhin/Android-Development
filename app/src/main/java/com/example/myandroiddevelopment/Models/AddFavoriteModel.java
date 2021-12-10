3d33b69400a8563b78b9462ebc41cd95f7561a40
yosmont <yann.osmont@epitech.eu>
2021-12-10 18:38:25 +0100

    MovieDetail - Fav & Rating

---
+++ app/src/main/java/com/example/myandroiddevelopment/Models/AddFavoriteModel.java
@@ -0,0 +1,12 @@
+package com.example.myandroiddevelopment.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AddFavoriteModel {
+    @SerializedName("media_type")
+    public String media_type;
+    @SerializedName("media_id")
+    public Integer media_id;
+    @SerializedName("favorite")
+    public Boolean favorite;
+}
