14c97e3d988770fa44ec77ce521b1e8db30074ca
chbouhin <christophe.bouhin@epitech.eu>
2021-12-16 19:54:18 +0100

    delete log when image error

--- app/src/main/java/com/example/myandroiddevelopment/Views/MovieDetail/MovieDetailFragment.java
+++ app/src/main/java/com/example/myandroiddevelopment/Views/MovieDetail/MovieDetailFragment.java
@@ -150,8 +150,6 @@ public class MovieDetailFragment extends Fragment {
                 InputStream in = new java.net.URL(urldisplay).openStream();
                 mIcon11 = BitmapFactory.decodeStream(in);
             } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
             }
             return mIcon11;
         }
